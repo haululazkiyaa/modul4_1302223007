43964ab82deaba646cce15249937eb015dfbcc58
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-03-14 11:02:24 +0700

    menambahkan classPosisiKarakterGame

---
+++ modul4_1302223007/PosisiKarakterGame.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302223007
+{
+    public enum KarakterState
+    {
+        Berdiri, Jongkok, Tengkurap, Terbang
+    }
+
+    public enum Trigger
+    {
+        TombolS, TombolW, TombolX
+    }
+
+    internal class PosisiKarakterGame
+    {
+        public class Transition
+        {
+            public KarakterState stateAwal;
+            public KarakterState stateAkhir;
+            public Trigger trigger;
+
+            public Transition(KarakterState stateAwal, KarakterState stateAkhir, Trigger trigger)
+            {
+                this.stateAwal = stateAwal;
+                this.stateAkhir = stateAkhir;
+                this.trigger = trigger;
+            }
+        }
+
+        Transition[] transisi =
+        {
+            new Transition(KarakterState.Berdiri, KarakterState.Terbang, Trigger.TombolW),
+            new Transition(KarakterState.Terbang, KarakterState.Berdiri, Trigger.TombolS),
+            new Transition(KarakterState.Terbang, KarakterState.Jongkok, Trigger.TombolX),
+            new Transition(KarakterState.Berdiri, KarakterState.Jongkok, Trigger.TombolS),
+            new Transition(KarakterState.Jongkok, KarakterState.Berdiri, Trigger.TombolW),
+            new Transition(KarakterState.Jongkok, KarakterState.Tengkurap, Trigger.TombolS),
+            new Transition(KarakterState.Tengkurap, KarakterState.Jongkok, Trigger.TombolW)
+        };
+
+        public KarakterState currState = KarakterState.Berdiri;
+
+        public KarakterState GetNextState(KarakterState stateAwal, Trigger trigger)
+        {
+            KarakterState stateAkhir = stateAwal;
+
+            for (int i = 0; i < transisi.Length; i++)
+            {
+                Transition perubahan = transisi[i];
+
+                if (stateAwal == perubahan.stateAwal && trigger == perubahan.trigger)
+                {
+                    stateAkhir = perubahan.stateAkhir;
+                }
+            }
+
+            return stateAkhir;
+        }
+
+        public void ActivateTrigger(Trigger trigger)
+        {
+            currState = GetNextState(currState, trigger);
+            
+            if (trigger == Trigger.TombolW && currState == KarakterState.Terbang)
+            {
+                Console.WriteLine("posisi take off");
+            }
+
+            if (trigger == Trigger.TombolX && currState == KarakterState.Jongkok)
+            {
+                Console.WriteLine("posisi landing");
+            }
+        }
+
+
+        public void SimulasiTrigger(Trigger trigger)
+        {
+            currState = GetNextState(currState, trigger);
+            Console.WriteLine(trigger);
+            Console.WriteLine("->" + currState);
+        }
+    }
+}
--- modul4_1302223007/Program.cs
+++ modul4_1302223007/Program.cs
@@ -1,7 +1,57 @@
-﻿internal class Program
+﻿using modul4_1302223007;
+using System.Xml.Linq;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // IMPLEMENTASI TABLE-DRIVEN
+        KodeBuah kodeBuah = new KodeBuah();
+
+        void printInfo(KodeBuah.Buah buah)
+        {
+            Console.WriteLine("[" + kodeBuah.getKodeBuah(buah) + "]" + buah);
+        }
+
+        Console.WriteLine("[Kode Buah] Nama Buah");
+        printInfo(KodeBuah.Buah.Apel);
+        printInfo(KodeBuah.Buah.Aprikot);
+        printInfo(KodeBuah.Buah.Alpukat);
+        printInfo(KodeBuah.Buah.Pisang);
+        printInfo(KodeBuah.Buah.Paprika);
+        printInfo(KodeBuah.Buah.Blackberry);
+        printInfo(KodeBuah.Buah.Ceri);
+        printInfo(KodeBuah.Buah.Kelapa);
+        printInfo(KodeBuah.Buah.Jagung);
+        printInfo(KodeBuah.Buah.Kurma);
+        printInfo(KodeBuah.Buah.Durian);
+        printInfo(KodeBuah.Buah.Anggur);
+        printInfo(KodeBuah.Buah.Melon);
+        printInfo(KodeBuah.Buah.Semangka);
+
+        Console.WriteLine("");
+
+        // IMPLEMENTASI STATE-BASED CONSTRUCTION
+        // NIM => 1302223007 % 3 = 2
+        Console.WriteLine("SIMULASI SESUAI NIM");
+        PosisiKarakterGame karakter = new PosisiKarakterGame(); 
+        Console.WriteLine("State Awal : " + karakter.currState);
+        // berdiri -> terbang
+        karakter.ActivateTrigger(Trigger.TombolW);
+        // terbang -> jongkok
+        karakter.ActivateTrigger(Trigger.TombolX);
+
+        Console.WriteLine("");
+        // SIMULASI SEMUA
+        Console.WriteLine("SIMULASI SEMUA STATE");
+        karakter.ActivateTrigger(Trigger.TombolW);
+        karakter.SimulasiTrigger(Trigger.TombolW);
+        karakter.SimulasiTrigger(Trigger.TombolS);
+        karakter.SimulasiTrigger(Trigger.TombolS);
+        karakter.SimulasiTrigger(Trigger.TombolW);
+        karakter.SimulasiTrigger(Trigger.TombolW);
+        karakter.SimulasiTrigger(Trigger.TombolX);
+        karakter.SimulasiTrigger(Trigger.TombolS);
+        karakter.SimulasiTrigger(Trigger.TombolW);
     }
 }
